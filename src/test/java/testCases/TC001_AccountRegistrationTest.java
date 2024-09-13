446c2bbcea2dd84763d10e85e3683495af34a5b6
shamnitjsr <shamnitjsr@gmail.com>
2024-09-13 19:44:16 +0530

    cross, sequentially and master execution by testng file

--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import pageObjects.AccountRegistrationPage;
 import pageObjects.HomePage;
 
-public class TC001_AccountRegistrationTest extends BaseClass{
+public class TC001_AccountRegistrationTest extends BaseClass {
 
 //	@Test(enabled = false)
 //	void verify_account_registration_01() {
@@ -34,27 +34,53 @@ public class TC001_AccountRegistrationTest extends BaseClass{
 	@Test
 	void verify_account_registration() {
 
-		HomePage hp = new HomePage(driver);
-		hp.clickMyAccount();
-		hp.clickRegister();
+		logger.info("***** Starting TC001_AccountRegistrationTest *****");
 
-		AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
-		regPage.setFirstName(randomString().toUpperCase());
-		regPage.setLastName(randomString().toUpperCase());
-		regPage.setEmail(randomString() + "@gmail.com"); // Random generated the email
-		regPage.setTelephone("7687546789");
-		
-		String password = randomAlphNumeric();
-		regPage.setPassword(password);
-		regPage.setConfirmPassowrd(password);
-		
-		regPage.setPrivacyPolicy();
-		regPage.clickContinue();
+		try {
+			HomePage hp = new HomePage(driver);
+			hp.clickMyAccount();
+			logger.info("Clicked on MyAccount Link...");
 
-		String confmsg = regPage.getConfirmationMsg();
+			hp.clickRegister();
+			logger.info("Clicked on Register Link...");
 
-		Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+			AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
+
+			logger.info("Providing Customer details...");
+
+			regPage.setFirstName(randomString().toUpperCase());
+			regPage.setLastName(randomString().toUpperCase());
+			regPage.setEmail(randomString() + "@gmail.com"); // Random generated the email
+			regPage.setTelephone("7687546789");
+
+			String password = randomAlphNumeric();
+			regPage.setPassword(password);
+			regPage.setConfirmPassowrd(password);
+
+			regPage.setPrivacyPolicy();
+			regPage.clickContinue();
+
+			logger.info("Validating expected message...");
+			String confmsg = regPage.getConfirmationMsg();
+
+			if (confmsg.equals("Your Account Has Been Created!")) {
+				Assert.assertTrue(true);
+			} else {
+
+				logger.error("Test Failed...");
+				logger.debug("Debug logs...");
+				Assert.assertTrue(false);
+
+			}
+
+		} catch (Exception e) {
+
+			Assert.fail();
+
+		}
+
+		logger.info("***** Finished TC001_AccountRegistrationTest *****");
+
 	}
 
-	
 }
